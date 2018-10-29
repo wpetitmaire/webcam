beabae3ec259595aa3abad846b4362f9605fdaf1
willy[MAC] <w.petitmaire@gmail.com>
2018-10-29 19:48:47 +0100

    Récupération des sources

---
+++ index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>Webcam lac</title>
+	<link rel="stylesheet" href="./styles/reset.css">
+	<link rel="stylesheet" href="./styles/positionnement.css">
+</head>
+
+<?php
+
+	$anneeCours = date('Y');
+	$moisCours = date('m');
+	$jourCours = date('d');
+
+	$picturesPath = '/sitewebcam/pictures/interval_snapshots/';
+	$lastSnapshot = 'lastsnap.jpg';
+
+?>
+
+
+<body>
+
+	<!-- Menu / titre -->
+	<div class='title flex-container-rows-center'>
+		<H1>Webcam du Lac</H1>
+	</div>
+
+	<section >
+		<div class='snapshot'>
+			<img src="<?=$picturesPath.$lastSnapshot?>" alt='Last snapshot' />
+		</div>
+	</section>
+	<div class='legend'>
+		<p>Date : <span id='snapDate'></span></p>
+	</div>
+
+	
+	<div>
+		<p>LOG : </p>
+		<p></p>
+	</div>
+
+</body>
+
+<!-- Contenu -->
+	<!-- <div class="corps flex-container-rows-center">
+
+		<section class="streaming flex-container-columns">
+			<img name="Webcam Lac" src="http://79.90.247.95:8081/"  alt="Vue sur le lac" title="Le lac" />
+		</section>
+	</div> -->
+
+
+</html>
