fd94395abf602adf845fbf3978189ae2b870a90c
willy[MAC] <w.petitmaire@gmail.com>
2018-11-14 21:47:49 +0100

    hover switch button

--- scripts/main.js
+++ scripts/main.js
@@ -20,18 +20,26 @@ let currentMode = readingmode.live.id;
  */
 function switchAction() {
 
+    console.log('--switchAction current: ' + currentMode + '--')
+
     switch(currentMode) {
 
         case readingmode.live.id :
-            jQuery('#imgLive').removeClass('display-none');
-            jQuery('#imgLastSnap').addClass('display-none');
+
+            console.log('   ##' + readingmode.live.id)
+
+            jQuery('#imgLive').addClass('display-none');
+            jQuery('#imgLastSnap').removeClass('display-none');
             currentMode = readingmode.lastimage.id;
             jQuery('#titreCam').html(readingmode.lastimage.title);
             break;
 
         case readingmode.lastimage.id :
-            jQuery('#imgLive').addClass('display-none');
-            jQuery('#imgLastSnap').removeClass('display-none');
+
+            console.log('   ##' + readingmode.lastimage.id)
+
+            jQuery('#imgLive').removeClass('display-none');
+            jQuery('#imgLastSnap').addClass('display-none');
             currentMode = readingmode.live.id;
             jQuery('#titreCam').html(readingmode.live.title);
             break;
